2e2dc2ceb1becb80763c7b34577dc86e6f36b2dd
DevPauloH <henriquedsg15@gmail.com>
2025-11-09 16:51:52 -0300

    Criando Entidade Produto

--- src/main/java/com/devph/tecnostory/Model/ProdutoEntity.java
+++ src/main/java/com/devph/tecnostory/Model/ProdutoEntity.java
@@ -1,6 +1,93 @@
 package com.devph.tecnostory.Model;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "produto")
 public class ProdutoEntity {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
 
+    private String nome;
+
+    @ManyToOne
+    @JoinColumn(name = "id_marca", nullable = false)
+    private MarcaEntity marca;
+
+    private String descricao;
+
+    private double preco;
+
+    private int quantidadeEstoque;
+
+    private String codigoDeBarras;
+
+    @ManyToOne
+    @JoinColumn(name = "id_categoria", nullable = false)
+    private CategoriaEntity categoria;
+
+    private ProdutoEntity() {}
+
+    public int getId() {
+        return id;
+    }
+
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public MarcaEntity getMarca() {
+        return marca;
+    }
+
+    public void setMarca(MarcaEntity marca) {
+        this.marca = marca;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+
+    public int getQuantidadeEstoque() {
+        return quantidadeEstoque;
+    }
+
+    public void setQuantidadeEstoque(int quantidadeEstoque) {
+        this.quantidadeEstoque = quantidadeEstoque;
+    }
+
+    public String getCodigoDeBarras() {
+        return codigoDeBarras;
+    }
+
+    public void setCodigoDeBarras(String codigoDeBarras) {
+        this.codigoDeBarras = codigoDeBarras;
+    }
+
+    public CategoriaEntity getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(CategoriaEntity categoria) {
+        this.categoria = categoria;
+    }
 }
