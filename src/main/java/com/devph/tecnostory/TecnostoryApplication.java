6b756596e73506a3b219f1a75c05c4f336a98bd3
DevPauloH <henriquedsg15@gmail.com>
2025-11-09 10:01:57 -0300

    Criando estrutura do TecnoStory Spring oot

---
+++ src/main/java/com/devph/tecnostory/TecnostoryApplication.java
@@ -0,0 +1,13 @@
+package com.devph.tecnostory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TecnostoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TecnostoryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devph/tecnostory/TecnostoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devph.tecnostory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TecnostoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
