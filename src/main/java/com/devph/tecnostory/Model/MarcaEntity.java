71eea901375687d29f2e6e0889a18f4e625b8c8c
DevPauloH <henriquedsg15@gmail.com>
2025-11-10 22:56:58 -0300

    update entidade Marca

--- src/main/java/com/devph/tecnostory/Model/MarcaEntity.java
+++ src/main/java/com/devph/tecnostory/Model/MarcaEntity.java
@@ -2,21 +2,24 @@ package com.devph.tecnostory.Model;
 
 import jakarta.persistence.*;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
 @Getter
 @Setter
+@NoArgsConstructor
 @Table(name = "marca")
 public class MarcaEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_marca", updatable = false)
     private int id;
 
+    @Column(name = "nome", nullable = false)
     private String nome;
 
-    public MarcaEntity() {}
 
     public MarcaEntity(String nome) {
         this.nome = nome;
