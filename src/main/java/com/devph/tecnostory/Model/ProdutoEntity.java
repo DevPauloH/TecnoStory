4bd601d82137f32bd51f30dc99f91584942341b8
DevPauloH <henriquedsg15@gmail.com>
2025-11-10 22:00:24 -0300

    update entidade Produto

--- src/main/java/com/devph/tecnostory/Model/ProdutoEntity.java
+++ src/main/java/com/devph/tecnostory/Model/ProdutoEntity.java
@@ -2,36 +2,45 @@ package com.devph.tecnostory.Model;
 
 import jakarta.persistence.*;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
+
+import java.math.BigDecimal;
 
 @Entity
 @Getter
 @Setter
+@NoArgsConstructor
 @Table(name = "produto")
 public class ProdutoEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_produto", updatable = false)
     private int id;
 
+    @Column(name = "nome", nullable = false)
     private String nome;
 
     @ManyToOne
     @JoinColumn(name = "id_marca", nullable = false)
     private MarcaEntity marca;
 
+    @Column(name = "descricao")
     private String descricao;
 
-    private double preco;
+    @Column(name = "preco", precision = 10, scale = 2, nullable = false)
+    private BigDecimal preco;
 
+    @Column(name = "quant_estoque", nullable = false)
     private int quantidadeEstoque;
 
+    @Column(name = "codigo_barras")
     private String codigoDeBarras;
 
     @ManyToOne
     @JoinColumn(name = "id_categoria", nullable = false)
     private CategoriaEntity categoria;
 
-    private ProdutoEntity() {}
 
 }
