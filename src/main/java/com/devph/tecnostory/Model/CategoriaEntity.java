7452176f721772f55ac423506e828fc2a7f2f3c4
DevPauloH <henriquedsg15@gmail.com>
2025-11-10 21:48:02 -0300

    update entidade Categoria

--- src/main/java/com/devph/tecnostory/Model/CategoriaEntity.java
+++ src/main/java/com/devph/tecnostory/Model/CategoriaEntity.java
@@ -2,26 +2,26 @@ package com.devph.tecnostory.Model;
 
 import jakarta.persistence.*;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
 @Getter
 @Setter
+@NoArgsConstructor
 @Table(name = "categoria")
 public class CategoriaEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_categoria", updatable = false)
     private int id;
 
-    @Column(nullable = false, unique = true)
+    @Column(name = "nome", nullable = false, unique = true)
     private String nome;
 
-    public CategoriaEntity() {
-    }
+
     public CategoriaEntity(String nome) {
         this.nome = nome;
     }
-
-
 }
