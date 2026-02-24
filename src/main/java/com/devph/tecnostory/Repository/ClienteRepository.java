7e43d910b821372f6d7bf575728728be0b82cc3a
DevPauloH <henriquedsg15@gmail.com>
2026-02-24 11:12:01 -0300

    Update ClienteRepository

--- src/main/java/com/devph/tecnostory/Repository/ClienteRepository.java
+++ src/main/java/com/devph/tecnostory/Repository/ClienteRepository.java
@@ -1,6 +1,5 @@
 package com.devph.tecnostory.Repository;
 
-import com.devph.tecnostory.DTO.ClienteRecordDTO;
 import com.devph.tecnostory.Model.ClienteEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
