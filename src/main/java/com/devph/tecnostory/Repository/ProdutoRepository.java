7b29fce102ef01bf778c00212eb57f717ae42a17
DevPauloH <henriquedsg15@gmail.com>
2026-02-25 10:22:06 -0300

    Criando ProdutoRepository

---
+++ src/main/java/com/devph/tecnostory/Repository/ProdutoRepository.java
@@ -0,0 +1,9 @@
+package com.devph.tecnostory.Repository;
+
+import com.devph.tecnostory.Model.ProdutoEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProdutoRepository extends JpaRepository<ProdutoEntity, Integer> {
+}
