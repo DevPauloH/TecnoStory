fc2ad599703051f6711459c62972fcffc0c86bb3
DevPauloH <henriquedsg15@gmail.com>
2026-03-01 00:26:30 -0300

    Criando vendaProdutoRepository

---
+++ src/main/java/com/devph/tecnostory/Repository/vendaProdutoRepository.java
@@ -0,0 +1,9 @@
+package com.devph.tecnostory.Repository;
+
+import com.devph.tecnostory.Model.vendaProdutoEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface vendaProdutoRepository extends JpaRepository<vendaProdutoEntity, Integer> {
+}
